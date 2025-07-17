d62b0a9753cfb90085affcc0700f1904481ef9ce
JonathanCofre88 <jon.cofre@duocuc.cl>
2025-07-17 01:45:29 +0000

    Cambio de nombre del archivo a ITEM1_Letra_A.py

---
+++ ITEM1_Letra_A.py
@@ -0,0 +1,10 @@
+# Listar los nombres de los integrantes del grupo.
+integrantes = [
+    "Jonathan Cofre",
+    "Daniel Rodriguez",
+    "Sofia Zamudio",
+]
+
+print("Integrantes del grupo:") # Imprimir nombres de los integrantes del grupo
+for nombres in integrantes:
+    print("-", nombres)
