420891347b2e13d8791fe55dc7dded6df6f607fb
JonathanCofre88 <jon.cofre@duocuc.cl>
2025-07-17 03:06:04 +0000

    Tipo de VLAN

---
+++ ITEM1_B.py
@@ -0,0 +1,10 @@
+vlan = int(input("Número de VLAN: "))
+
+if vlan >= 1 and vlan <= 1005:
+    print("Rango normal")
+else:
+    if vlan >= 1006 and vlan <= 4094:
+        print("Rango extendido")
+    else:
+        print("VLAN no válida")
+        
